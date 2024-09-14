94f55be51a16a14cebd51b69d207d281d18480f2
Aviv Kandabi <aviv.k@myzorro.co>
2024-09-14 19:16:31 +0300

    Save

--- distance/src/main.ts
+++ distance/src/main.ts
@@ -1,13 +1,13 @@
 import * as THREE from "three";
 
-const getDistance = (a: number, b: number): number => {
-  return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+const getDistance = (a: number[], b: number[]): number => {
+  return Math.sqrt(Math.pow(b[0] - a[0], 2) + Math.pow(b[1] - a[1], 2));
 };
 
-const getNormalizedVector = (a: number, b: number) => {
-  const distance = getDistance(a, b) || 1;
-  return [a / distance, b / distance];
-};
+// const getNormalizedVector = (a: number, b: number) => {
+//   const distance = getDistance(a, b) || 1;
+//   return [a / distance, b / distance];
+// };
 
 window.onload = () => {
   const scene = new THREE.Scene();
@@ -50,39 +50,68 @@ window.onload = () => {
   }
 
   function initLines() {
-    const a = [0, 0];
-    const b = [0, 0];
-    const points = [
-      new THREE.Vector2(a[0], a[1]),
-      new THREE.Vector2(b[0], b[1]),
-    ];
-    const geometry = new THREE.BufferGeometry().setFromPoints(points);
-    scene.add(
-      new THREE.Line(
-        geometry,
-        new THREE.LineBasicMaterial({
-          color: 0x0000ff,
-        }),
-      ),
+    // const a = [0, 0];
+    // const b = [0, 0];
+    // const points = [
+    //   new THREE.Vector2(a[0], a[1]),
+    //   new THREE.Vector2(b[0], b[1]),
+    // ];
+    // const geometry = new THREE.BufferGeometry().setFromPoints(points);
+    // scene.add(
+    //   new THREE.Line(
+    //     geometry,
+    //     new THREE.LineBasicMaterial({
+    //       color: 0x0000ff,
+    //     }),
+    //   ),
+    // );
+
+    // const aToB = [b[0] - a[0], b[1] - a[1]];
+    // const normalizedAToB = getNormalizedVector(aToB[0], aToB[1]);
+
+    // const points2 = [
+    //   new THREE.Vector2(a[0], a[1]),
+    //   new THREE.Vector2(a[0] + normalizedAToB[0], a[1] + normalizedAToB[1]),
+    // ];
+
+    // const geometry2 = new THREE.BufferGeometry().setFromPoints(points2);
+    // scene.add(
+    //   new THREE.Line(
+    //     geometry2,
+    //     new THREE.LineBasicMaterial({
+    //       color: 0xff00ff,
+    //     }),
+    //   ),
+    // );
+
+    const a = [1, 2.72];
+    const aMesh = new THREE.Mesh(
+      new THREE.CircleGeometry(0.05, 32),
+      new THREE.MeshBasicMaterial({
+        color: 0x0000ff,
+      }),
     );
 
-    const aToB = [b[0] - a[0], b[1] - a[1]];
-    const normalizedAToB = getNormalizedVector(aToB[0], aToB[1]);
+    aMesh.position.set(a[0], a[1], 0);
 
-    const points2 = [
-      new THREE.Vector2(a[0], a[1]),
-      new THREE.Vector2(a[0] + normalizedAToB[0], a[1] + normalizedAToB[1]),
-    ];
+    scene.add(aMesh);
 
-    const geometry2 = new THREE.BufferGeometry().setFromPoints(points2);
-    scene.add(
-      new THREE.Line(
-        geometry2,
-        new THREE.LineBasicMaterial({
-          color: 0xff00ff,
-        }),
-      ),
-    );
+    const circleRadius = 2;
+    const circleOrigin = [0, 0];
+
+    const distanceCircleToA = getDistance(a, circleOrigin);
+    const isInside = distanceCircleToA < circleRadius;
+
+    const circleGeometry = new THREE.CircleGeometry(circleRadius, 32);
+    const material = new THREE.MeshBasicMaterial({
+      color: isInside ? 0x00ff00 : 0xff0000,
+      transparent: true,
+      opacity: 0.2,
+    });
+    const circle = new THREE.Mesh(circleGeometry, material);
+
+    circle.position.set(circleOrigin[0], circleOrigin[1], 0);
+    scene.add(circle);
   }
 
   initGrid();
